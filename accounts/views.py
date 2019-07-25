5d23a5e355642563657977c0d23828eb21e6d348
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-07-25 11:40:29 +0300

    Setting up the templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+def home(request):
+    return render(request, 'accounts/home.html')
 
 # Create your views here.
