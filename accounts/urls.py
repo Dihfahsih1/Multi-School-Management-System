79ac4f7a188f32b659e2b3bf0a8ed5c3ad6dd497
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-07-27 15:27:46 +0300

    fixed delete view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ url(r'^$', views.home, name="home"),
 url(r'^login', views.login, name="login"),
 url(r'^createclassinformation', views.createclassinformation, name="createclassinformation"),
 url(r'^editclassinformation/(?P<pk>\d+)', views.editclassinformation, name="editclassinformation"),
-url(r'^deleteclassinformation', views.deleteclassinformation, name="deleteclassinformation"),
+url(r'^deleteclassinformation/(?P<pk>\d+)', views.deleteclassinformation, name="deleteclassinformation"),
 url(r'^viewclassinformation', views.viewclassinformation, name="viewclassinformation"),
 url(r'^logout', views.login, name="logout")
 ]
