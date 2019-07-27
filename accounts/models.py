e4f2fa059d0412fd116baee6513f79305cee286f
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-07-27 20:13:10 +0300

    creating Section table in the database

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,14 @@ class Classinformation(models.Model):
     ClassTeacher = models.CharField(max_length=30, blank=True)
     TotalStudents = models.CharField(max_length=30, blank=True)
     NumberOfSections = models.CharField(max_length=30, blank=True)
+    def __str__(self):
+        return 'Class Name:{0} Students Number:{1}'.format(self.ClassName, self.TotalStudents)
+
+class Sectioninformation(models.Model):
+    NameOfClass = models.ForeignKey(Classinformation, on_delete=models.PROTECT, blank=True, null=True)
+    SectionName = models.CharField(max_length=130)
+    SectionTeacher = models.CharField(max_length=30, blank=True)
+    NumberOfStudents = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.NameOfClass
