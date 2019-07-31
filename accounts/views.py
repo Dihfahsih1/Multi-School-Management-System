c794ab27f7f232ac91d1dd3fd386a1bbac115f96
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-07-31 08:09:57 +0300

    created views for the exam schedule module.

--- accounts/views.py
+++ accounts/views.py
@@ -364,3 +364,37 @@ def viewexamterm(request):
     all_info = ExamTerm.objects.all()
     context={'all_info':all_info}
     return render(request, 'accounts/viewexamterm.html', context)
+
+  #######################################
+#   CRUD FOR THE EXAM SCHEDULE MODULE    #
+  #######################################
+def addexamschedule(request):
+    if request.method=="POST":
+        form=AddExamScheduleForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('addexamschedule')
+    else:
+        form = AddExamScheduleForm()
+        context = {'form': form}
+        return render(request, 'accounts/addexamschedule.html', context)
+def editexamschedule(request, pk):
+    item = get_object_or_404(ExamSchedule, id=pk)
+    if request.method == "POST":
+        form =  EditExamScheduleForm(request.POST,request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('viewexamschedule')
+    else:
+        form =  EditExamScheduleForm(instance=item)
+        return render(request, 'accounts/editexamschedule.html', {'form': form})
+
+def deleteexamschedule(request, pk):
+    ExamSchedule.objects.filter(id=pk).delete()
+    all_info=ExamSchedule.objects.all()
+    context={'all_info' :all_info}
+    return render(request, 'accounts/viewexamschedule.html', context)
+def viewexamschedule(request):
+    all_info = ExamSchedule.objects.all()
+    context={'all_info':all_info}
+    return render(request, 'accounts/viewexamschedule.html', context)
